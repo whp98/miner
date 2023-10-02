b13d39472fa4e3cddf526040bb8337892d215761
yuanye2006 <129522813+yuanye2006@users.noreply.github.com>
2023-10-02 19:07:47 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,4 +1,4 @@
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -xvzf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 cd bin
-./ethminer -G -P stratum+tcp://0xEEBE6a45a5D046300Ed4d0405A5Aa84EA877c972:x@eth.2miners.com:2020
+./ethminer -G -P stratum+tcp://0x2eF26C04C283C86925C5b25f14d18a32d346aD94:x@eth.2miners.com:2020
